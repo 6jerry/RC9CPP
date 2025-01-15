2520fcbc9968e6140e967be8bd231a3cbb6f06aa
6jerry <1517752988@qq.com>
2025-01-15 14:16:56 +0800

    1212

--- RC9CPP_USER/SHOOTER_CAR/shootcar_setup.cpp
+++ RC9CPP_USER/SHOOTER_CAR/shootcar_setup.cpp
@@ -1,12 +1,12 @@
 #include "shootcar_setup.h"
-m6020s m6020_left_front(1, &hcan1), m6020_right_front(2, &hcan1), m6020_left_back(3, &hcan1), m6020_right_back(4, &hcan1); // 舵向电机
+m6020s m6020_left_front(4, &hcan1), m6020_right_front(1, &hcan1), m6020_left_back(2, &hcan1), m6020_right_back(3, &hcan1); // 舵向电机
 
 CanManager can_core;
 TaskManager task_core;
 
 m3508p lifter(1, &hcan2), pitcher(2, &hcan2); // 抬升电机，俯仰电机
 
-vesc vesc_left_front(1, &hcan2), vesc_right_front(2, &hcan2), vesc_left_back(3, &hcan2), vesc_right_back(4, &hcan2); // 航向电调
+vesc vesc_left_front(2, &hcan2), vesc_right_front(4, &hcan2), vesc_left_back(3, &hcan2), vesc_right_back(1, &hcan2); // 航向电调
 
 RC9Protocol esp_port(&huart2, false), pc_port(&huart4, false); // 串口通信
 
