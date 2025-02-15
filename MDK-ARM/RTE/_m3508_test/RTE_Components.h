5936f922f86a57fee07b4d2e10b6ace1038c45d0
6jerry <1517752988@qq.com>
2025-02-15 21:59:27 +0800

    sd

--- MDK-ARM/RTE/_m3508_test/RTE_Components.h
+++ MDK-ARM/RTE/_m3508_test/RTE_Components.h
@@ -16,6 +16,12 @@
  */
 #define CMSIS_device_header "stm32f4xx.h"
 
+/* Keil::Compiler&ARM Compiler:Event Recorder&DAP@1.5.1 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil::Compiler&ARM Compiler:I/O:STDOUT&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_BKPT     /* Compiler I/O: STDOUT Breakpoint */
 
 
 #endif /* RTE_COMPONENTS_H */
