3cd4c6fe6077e237428cf64e81367abf50063690
Karula <a1619481571@outlook.com>
2025-03-18 11:43:22 +0800

    改keil

--- MDK-ARM/m3508_test/taskmanager.d
+++ MDK-ARM/m3508_test/taskmanager.d
@@ -1,8 +1,3 @@
 m3508_test/taskmanager.o: ..\RC9CPP_API\HARDWARE\TaskManager.cpp \
   ..\RC9CPP_API\HARDWARE\TaskManager.h \
-  ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os2.h \
-  G:\Keil_v5\ARM\ARMCLANG\include\libcxx\stdint.h \
-  G:\Keil_v5\ARM\ARMCLANG\include\libcxx\__config \
-  G:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  G:\Keil_v5\ARM\ARMCLANG\include\libcxx\stddef.h \
-  G:\Keil_v5\ARM\ARMCLANG\include\stddef.h
+  ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os2.h
