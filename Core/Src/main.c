816e3bc94fd80d0fd27fdc259e218b8299148aa3
6jerry <1517752988@qq.com>
2025-03-21 15:16:43 +0800

    233

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -32,7 +32,7 @@
 // #include "r2n_setup.h"
 
 // #include "m6020_adjust.h"
-#include "chassis_test.h"
+#include "YunBall_test.h"
 
 /* USER CODE END Includes */
 
@@ -131,7 +131,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   // MX_USB_DEVICE_Init();
 
-  chassis_move_test();
+  yunball_test_setup();
   while (1)
   {
     /* USER CODE END WHILE */
