e9aec37977b37b84ad88e9e3b32d150846e0df70
6jerry <1517752988@qq.com>
2025-04-29 14:02:30 +0800

    11

--- RC9CPP_USER/RC2025/auto_lock.cpp
+++ RC9CPP_USER/RC2025/auto_lock.cpp
@@ -58,7 +58,8 @@ void auto_lock_test::xbox_on()
     center_point.x = 5.841f;
     center_point.y = 0.75f;
     nor_control.ConfigAll(1.0f, 0.0f, 0.02f, 0.0f, 1.0f, 0.005f, 0.0f);
-    imu_ptr->imu_relocate(0.5f, 0.5f, 30.0f);
+   // imu_ptr->imu_relocate(0.5f, 0.5f, 30.0f);
+    init_locate();
 }
 auto_lock_test::auto_lock_test(imu *imu_ptr_)
 {
