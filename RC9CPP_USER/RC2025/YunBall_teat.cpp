5372a1382cd572b93abbb9aa81722b526ca627c6
Karula <a1619481571@outlook.com>
2025-05-17 19:20:38 +0800

    放球优化

--- RC9CPP_USER/RC2025/YunBall_teat.cpp
+++ RC9CPP_USER/RC2025/YunBall_teat.cpp
@@ -68,7 +68,7 @@ extern "C"
         // PD7 夹爪 PG10 射球 PG11气阀 PE2 棘轮
         autoshooter.add_trigger(GPIOF, GPIO_PIN_5, GPIOE, GPIO_PIN_2, GPIOG, GPIO_PIN_10);
         autoshooter.add_motor(&m8080, &pithcer);
-        autoshooter.add_plan_info(600.0f, 400.0f, 1400.0f, 600.0f, 200.0f);
+        autoshooter.add_plan_info(600.0f, 400.0f, 1600.0f, 600.0f, 200.0f);
         autoshooter.addport(&receive_port);
 			
         xbox_test.add_auto_shooter(&autoshooter);
