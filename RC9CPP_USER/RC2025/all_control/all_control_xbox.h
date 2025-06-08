c3b02841e0ff0ae46e62a720e8bd0674bd014281
6jerry <1517752988@qq.com>
2025-06-08 17:11:08 +0800

    1

---
+++ RC9CPP_USER/RC2025/all_control/all_control_xbox.h
@@ -0,0 +1,17 @@
+#ifndef ALL_CONTROL_XBOX_H
+#define ALL_CONTROL_XBOX_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "xbox.h"
+#include "RC9Protocol.h"
+#include "imu.h"
+#ifdef __cplusplus
+}
+#endif
+#ifdef __cplusplus
+
+#endif
+#endif
