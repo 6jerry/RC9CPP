d80c0119ac0ccadb7af689c9b0f663891885f65f
Weicanhui <3123002393@mail2.gdut.edu.cn>
2025-07-03 18:22:32 +0800

    7-3下午三分拟合函数

--- RC9CPP_USER/RC2025/shoot_module/auto_shooter.h
+++ RC9CPP_USER/RC2025/shoot_module/auto_shooter.h
@@ -72,9 +72,9 @@ private:
     float target_rpm = 10.0f;
     float test_dis;
     float offest = 0.0f;
-    float a = 0.0381f;
-    float b = 0.3617f;
-    float c = 0.0735f;
+    float a = 0.4844f;
+    float b = 0.5588f;
+    float c = -0.5108f;
 
     GPIO_TypeDef *shooter_port = nullptr, *stop_port = nullptr;
     uint16_t shooter_pin = 0, stop_pin = 0;
