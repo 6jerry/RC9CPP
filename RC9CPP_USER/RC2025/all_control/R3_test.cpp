19b8a80f6142a0c505537ec49b8eb943bccc4929
Yang <yangminhao_ymh@126.com>
2025-07-07 21:20:18 +0800

    R3底盘
    R3

--- RC9CPP_USER/RC2025/all_control/R3_test.cpp
+++ RC9CPP_USER/RC2025/all_control/R3_test.cpp
@@ -8,7 +8,7 @@ RC9Protocol esp_port(uart, &huart3), debug_port(uart, &huart4);
 vesc shoot_1(vesc_id_1, &hfdcan1, 7.0f, 1.0f);
 vesc shoot_2(vesc_id_2, &hfdcan1, 7.0f, 1.0f);
 
-m3508p front_left_motor(2, &hfdcan1), front_right_motor(3, &hfdcan1), back_left_motor(1, &hfdcan1), back_right_motor(4, &hfdcan1);
+m3508p front_left_motor(dji_id_4, &hfdcan3), front_right_motor(dji_id_2, &hfdcan3), back_left_motor(dji_id_3, &hfdcan3), back_right_motor(dji_id_1, &hfdcan3);
 RoboChassis robot_chassis(omni4_chassis);
 chassis_info omni4_info = {0.0719f, 0.0f, 0.0f, 0.2425f, 0.0f, 0.0f};
 
@@ -26,12 +26,17 @@ extern "C"
         debug_port.startUartReceiveIT();
         debug_port.initQueue();
 
+		front_left_motor.rpm_control.config_all(32.0f, 0.76f, 8.6f, 106.0f, 20000.0f, 5.0f);
+		front_right_motor.rpm_control.config_all(32.0f, 0.76f, 8.6f, 106.0f, 20000.0f, 5.0f);
+		back_left_motor.rpm_control.config_all(32.0f, 0.76f, 8.6f, 106.0f, 20000.0f, 5.0f);
+		back_right_motor.rpm_control.config_all(32.0f, 0.76f, 8.6f, 106.0f, 20000.0f, 5.0f);
+		
         test_xbox.addport(&esp_port);
         test_xbox.gate.add_io_interrupt(GPIOD, GPIO_PIN_14);
         test_xbox.add_chassis(&robot_chassis);
-        robot_chassis.add4_motors(&front_left_motor, &front_right_motor, &back_left_motor, &back_right_motor);
+        robot_chassis.add4_motors(&back_right_motor, &front_right_motor, &front_left_motor, &back_left_motor);
         robot_chassis.config(omni4_info);
-        robot_chassis.pointtrack_config(0.76f, 0.0f, 0.25f, 0.0f, 5.0f, 0.008f, 0.0f);
+        robot_chassis.pointtrack_config(0.76f, 0.0f, 0.25f, 0.0f, 5.0f, 0.008f, 0.0f); 
         // robotomi4_chassis.add_imu(&position_sensor);
         robot_chassis.yawadjuster_config(0.12f, 0.0f, 0.004f, 0.0f, 5.0f, 0.1f, 0.5f);
 
--- RC9CPP_USER/RC2025/all_control/R3_testxbox.cpp
+++ RC9CPP_USER/RC2025/all_control/R3_testxbox.cpp
@@ -25,12 +25,24 @@ void R3_xbox::mode_1()
 void R3_xbox::mode_2()
 {
 
-	// Vector2D tvel_((5.0f * xbox_msgs.joyLHori_map), (5.0f * xbox_msgs.joyLVert_map));
-
-	// set_RobotVel(tvel_, 0);
-	// set_RobotW(-(2.0f * xbox_msgs.joyRHori_map), 0);
 	gate.flag = 0;
-			shoot_motor_1->send_rpm(move_rpm * xbox_msgs.joyLVert_map);
+	Vector2D tvel_((-(5.0f * xbox_msgs.joyLHori_map)), (5.0f * xbox_msgs.joyLVert_map));
+
+	set_RobotVel(tvel_, 0);
+	set_RobotW(-(2.0f * xbox_msgs.joyRHori_map), 0);
+
+		//shoot_motor_1->set_rpm(move_rpm * xbox_msgs.joyLVert_map);
+	 // shoot_motor_2->set_current(shoot_motor_1->get_target_current());
+	
+	 //shoot_motor_1->send_rpm(0.0f);
+	// shoot_motor_2->send_rpm(0.0f);
+}
+
+void R3_xbox::mode_3()
+{
+
+	gate.flag = 0;
+		shoot_motor_1->send_rpm(move_rpm * xbox_msgs.joyLVert_map);
 		shoot_motor_2->send_rpm(move_rpm * xbox_msgs.joyLVert_map);
 		//shoot_motor_1->set_rpm(move_rpm * xbox_msgs.joyLVert_map);
 	 // shoot_motor_2->set_current(shoot_motor_1->get_target_current());
@@ -39,10 +51,14 @@ void R3_xbox::mode_2()
 	// shoot_motor_2->send_rpm(0.0f);
 }
 
+
 void R3_xbox::not_start()
 {
 	shoot_motor_1->send_rpm(0.0f);
 	shoot_motor_2->send_rpm(0.0f);
+	Vector2D zero(0 , 0);
+	set_RobotVel(zero, 0);
+	set_RobotW (0, 0);
 }
 
 photogate_shoot::photogate_shoot()
