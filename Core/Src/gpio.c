1bd45935456c63872d2bc2a3afc684b663b80ae9
Karula <a1619481571@outlook.com>
2025-07-21 15:06:40 +0800

    111

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -73,7 +73,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PF9 */
   GPIO_InitStruct.Pin = GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA2 PA8 */
