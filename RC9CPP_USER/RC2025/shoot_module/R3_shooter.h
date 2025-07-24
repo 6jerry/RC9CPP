2352438fd5a3e524564ab035a7c1c8482661a8da
yqy <yqy963132@outlook.com>
2025-07-24 18:33:03 +0800

    111

--- RC9CPP_USER/RC2025/shoot_module/R3_shooter.h
+++ RC9CPP_USER/RC2025/shoot_module/R3_shooter.h
@@ -123,6 +123,7 @@ public:
     void set_auto_byrpm(uint8_t mode_, float rpm);
     void set_hand(float rpm);
     int set_auto_byFitter(uint8_t mode_, float r);
+    void set_auto_byCameraFitter(float camera_r);
     void set_lift(float dis);
     float calc_camera(float camera_r);
     float calc(float r);
