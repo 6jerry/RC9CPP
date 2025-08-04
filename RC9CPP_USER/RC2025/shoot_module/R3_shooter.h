97c7f9e1d5ea72f9f617fa88cd4778c57a34f36c
6jerry <1517752988@qq.com>
2025-08-04 18:56:38 +0800

    11

--- RC9CPP_USER/RC2025/shoot_module/R3_shooter.h
+++ RC9CPP_USER/RC2025/shoot_module/R3_shooter.h
@@ -94,8 +94,8 @@ private:
     float b = 0.5614f;
     float c = 835.2703f;
     float v;
-    float offset = 0.0f;
-    float camera_offset = 0.002f;
+    float offset = -25.0f;
+    float camera_offset = -20.0f;
     float pass_offset = 125.0f;
     float s_dis;
 
